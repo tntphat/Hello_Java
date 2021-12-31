fe38835ed1c787e8e4ec66100e267aec044b71f6
InternshipYOOT <tonguyentanphat@gmail.com>
2021-12-31 23:21:37 +0700

    4th commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String args[]) {
-        System.out.println("edit hello ava");
+        System.out.println("haizzzzz lam cai seminar nay met ge");
     }
 }
